272d66a4566330d00ff3df1befa7585a65a6f1a5
Kontrolix <damien.couppe@mergify.com>
2023-05-26 10:19:52 +0200

    add cmd

---
+++ cmd.sh
@@ -0,0 +1,15 @@
+export $branch=5
+git checkout main
+git pull
+git checkout -b "A$branch"
+git touch "add_A$branch"
+git add "add_A$branch"
+git commit -a -m "Add A$branch"
+git checkout -b "B$branch"
+git touch "add_B$branch"
+git add "add_B$branch"
+git commit -a -m "Add B$branch"
+git checkout -b "C$branch"
+git touch "add_C$branch"
+git add "add_C$branch"
+git commit -a -m "Add C$branch"
