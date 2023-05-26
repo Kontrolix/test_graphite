9348eeca233f26d0913ff59737d40088bf7c00c4
Kontrolix <damien.couppe@mergify.com>
2023-05-26 10:21:15 +0200

    typo

--- cmd.sh
+++ cmd.sh
@@ -2,14 +2,14 @@ export branch=5
 git checkout main
 git pull
 git checkout -b "A$branch"
-git touch "add_A$branch"
+touch "add_A$branch"
 git add "add_A$branch"
 git commit -a -m "Add A$branch"
 git checkout -b "B$branch"
-git touch "add_B$branch"
+touch "add_B$branch"
 git add "add_B$branch"
 git commit -a -m "Add B$branch"
 git checkout -b "C$branch"
-git touch "add_C$branch"
+touch "add_C$branch"
 git add "add_C$branch"
 git commit -a -m "Add C$branch"
